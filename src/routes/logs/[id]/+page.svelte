fd638fd179b25717ec56fe90b268cbcc2b5116c1
Krishaay Jois <krishaayjois@gmail.com>
2023-12-28 21:36:01 +0530

    WIP

--- src/routes/logs/[id]/+page.svelte
+++ src/routes/logs/[id]/+page.svelte
@@ -1,5 +1,19 @@
 <script lang="ts">
     import type { PageData } from "../$types";
-
     export let data: PageData;
+    import { firestore } from "$lib/firebase";
+    import { collection, where, query, orderBy } from "firebase/firestore";
+
+    import { collectionStore } from "sveltefire";
+    const logsCollection = collection(firestore, "logs");
+    const q = query(
+        logsCollection,
+        where("userId", "==", data.userId),
+        orderBy("timestamp", "desc"),
+    );
+    const userLogs = collectionStore(firestore, q);
 </script>
+
+{#each $userLogs as log}
+    <span>log['userId']</span>
+{/each}
