0f2353ddf1a220f0e27fd9bbb09786cbce594441
Krishaay Jois <krishaayjois@gmail.com>
2024-01-01 10:42:03 +0530

    Log Viewer Complete

--- src/routes/logs/[id]/+page.svelte
+++ src/routes/logs/[id]/+page.svelte
@@ -8,8 +8,11 @@
         X,
     } from "lucide-svelte";
     import type { PageData } from "../$types";
+    import Time from "svelte-time";
     export let data: PageData;
-    import { Doc } from "sveltefire";
+    import { Doc, docStore } from "sveltefire";
+    import { firestore } from "$lib/firebase";
+    const docStr = docStore(firestore, `users/${data.userId}`);
     const sampleLogs = [
         {
             timestamp: Date.now(),
@@ -43,50 +46,105 @@
         }
         return newList;
     };
+    function toDateTime(secs) {
+        var t = new Date(1970, 0, 1); // Epoch
+        const tzsecs = secs + 19800;
+        t.setSeconds(tzsecs);
+
+        // Set the time zone to Asia/Kolkata
+        var options = { timeZone: "Asia/Kolkata" };
+
+        // Format the date and time using toLocaleString with options
+        var formattedDateTime = t.toLocaleString("en-US", options);
+
+        return formattedDateTime;
+    }
+    function calculateTimeDifference(startDate, endDate) {
+        // subtract the two dates
+        var difference = Math.abs(endDate - startDate);
+        let secDiff = difference / 10000;
+        const days = Math.floor(secDiff / 60 / 60 / 24);
+        secDiff -= days * 60 * 60 * 24;
+        const hours = Math.floor(secDiff / 60 / 60);
+        secDiff -= hours * 60 * 60;
+        const minutes = Math.floor(secDiff / 60);
+        secDiff -= minutes * 60;
+        const seconds = Math.floor(secDiff);
+        let timeDiffStr = "";
+        if (days > 0) {
+            timeDiffStr += `${days}d ${hours}h ${minutes}m ${seconds}s`;
+        } else {
+            if (hours > 0) {
+                timeDiffStr += `${hours}h ${minutes}m ${seconds}s`;
+            } else {
+                if (minutes > 0) {
+                    timeDiffStr += `${minutes}m ${seconds}s`;
+                } else {
+                    timeDiffStr += `${seconds}s`;
+                }
+            }
+        }
+        return timeDiffStr;
+    }
 </script>
 
-<center>
-    <h1 class="text-3xl">
-        Logs for <span class="text-secondary">kryo</span>
-    </h1>
-</center>
-<br />
-<ul class="timeline timeline-vertical">
-    {#each reverseList(sampleLogs) as log, index}
-        <li>
-            {#if index !== 0}
-                <hr />
-            {/if}
-            <div class="timeline-start timeline-box">04/01/2024 • 20:00</div>
-            <div class="timeline-middle">
-                {#if log.type === "pass"}
-                    <PlusCircle class="text-success" />
-                {:else}
-                    <CircleSlash class="text-error" />
+{#if $docStr == undefined}
+    <center>
+        <h1 class="text-3xl">User doesn't exist</h1>
+    </center>
+{:else}
+    <center>
+        <h1 class="text-3xl">
+            Logs for <span class="text-secondary">{$docStr.username}</span>
+        </h1>
+    </center>
+    <br />
+    <ul class="timeline timeline-vertical">
+        {#each reverseList($docStr.logs) as log, index}
+            <li>
+                {#if index !== 0}
+                    <hr />
                 {/if}
-            </div>
-            <div class="timeline-end timeline-box">
-                {#if log.type === "pass"}
-                    Advanced to Level {log.nextLevel}
-                {:else}
-                    <span class="font-bold"
-                        >Failed Level {log.currentLevel}</span
-                    ><br />
-                    Expected: <i>{log.expectedAnswer}</i><br />
-                    Submitted: <i>{log.submittedAnswer}</i>
-                {/if}
-            </div>
+                <div class="timeline-start timeline-box">
+                    <Time
+                        format={"MMMM D, YYYY · h:mm A"}
+                        timestamp={toDateTime(log.timestamp / 1000)}
+                    ></Time>
+                </div>
+                <div class="timeline-middle">
+                    {#if log.type === "pass"}
+                        <PlusCircle class="text-success" />
+                    {:else}
+                        <CircleSlash class="text-error" />
+                    {/if}
+                </div>
+                <div class="timeline-end timeline-box">
+                    {#if log.type === "pass"}
+                        Advanced to Level {log.nextLevel}
+                    {:else}
+                        <span class="font-bold"
+                            >Failed Level {log.currentLevel}</span
+                        ><br />
+                        Expected: <i>{log.expectedAnswer}</i><br />
+                        Submitted: <i>{log.submittedAnswer}</i>
+                    {/if}
+                </div>
 
-            {#if index !== sampleLogs.length - 1}
-                <hr />
-            {/if}
-        </li>
-        {#if index !== sampleLogs.length - 1}
-            <div class="timeline-end font-mono">0m 0s</div>
-        {/if}
-    {/each}
-</ul>
-<!-- <Doc ref={`users/${data.userId}`} let:data>
+                {#if index !== sampleLogs.length - 1}
+                    <hr />
+                {/if}
+            </li>
+            <!-- {#if index !== sampleLogs.length - 1}
+                <div class="timeline-end font-mono">
+                    {calculateTimeDifference(
+                        sampleLogs[index + 1].timestamp,
+                        log.timestamp,
+                    )}
+                </div>
+            {/if} -->
+        {/each}
+    </ul>
+    <!-- <Doc ref={`users/${data.userId}`} let:data>
     <h1>Logs for {data.username}</h1>
     <ul>
         {#each data["logs"] as log}
@@ -96,3 +154,4 @@
         {/each}
     </ul>
 </Doc> -->
+{/if}
